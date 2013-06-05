bd54b28d4ab9291a56310f5ee44ed9fa08500680
Lucas Suggs <lucas.suggs@gmail.com>
2013-06-04 20:36:47 -0700

    Create the initial project/solution and set up the assembly info.

---
+++ ActiveCampaign/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ActiveCampaign")]
+[assembly: AssemblyDescription("An open-source .NET library that will interface with the ActiveCampaign REST API.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SugarHigh Software")]
+[assembly: AssemblyProduct("ActiveCampaign API")]
+[assembly: AssemblyCopyright("Copyright ©  2013, SugarHigh Software")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e3cf9d9a-fcd3-44e9-95f9-d9a1d192dc9f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
